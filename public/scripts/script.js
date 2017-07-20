62f87169ce80934a94fab6dc6513098b220195d9
maxiliarias <maxiliarias@gmail.com>
2017-07-19 17:56:11 -0700

    modified scripts file closing modal and css source file

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,16 @@
 
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myVenues");
-var x = document.getElementsByClassName("close")[0];
+var x = document.getElementsByClassName("close");
 
 
 // When the user clicks on (x), close the modal
-x.onclick = function() {
+x.onClick = function() {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onClick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
