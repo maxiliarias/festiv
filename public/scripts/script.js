ada5a48e2eb58907549966f287902fd26d1b0785
maxiliarias <maxiliarias@gmail.com>
2017-08-11 18:53:31 -0700

    ok

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,33 +1,40 @@
 $( document ).ready(function() {
-    console.log( "jQuery ready!" );
 
+    // HOME PAGE
+    // Make post requests to searches that show these favorite venues
+        $('#topvenue1').click(function(){
+            $( "#topvenue1" ).submit();
+        })
+        $('#topvenue2').click(function(){
+            $( "#topvenue2" ).submit();
+        })
+        $('#topvenue3').click(function(){
+            $( "#topvenue3" ).submit();
+        })
+
+    // MODAL ON LIST PAGE
     //When you click "x" button, closes the modal in venue list view
-    $('.close').on('click', function(){
-      $(".viewVenues").hide()
-    })
+        // $('.close').on('click', function(){
+        //   $(".viewVenues").hide()
+        // })
     //When you tag a venue to an event, close the modal
-    $('.taggedEvent').on('click', function(){
-      $(".viewVenues").hide()
-    })
-    // for when venues renders nothing
-    if(window.location.href.split("?")[1] === "needsearch=true"){
-        alert("Please make a new search first")
-    }
+        $('.taggedEvent').on('click', function(){
+          alert("Great! You saved the venue!")
+          $(".viewVenues").hide()
+        })
+    //If not loggedin, show modal requesting user log in
 
+    // for when Most Recent Search renders nothing
+        if(window.location.href.split("?")[1] === "needsearch=true"){
+            alert("Please make a new search first")
+        }
+
+    //QUOTE PAGE
     $( "#request-bids" ).click(function() {
         $( "#contactBtn" ).trigger("click");
     });
 
-    // Make post requests to these searches
-    $('#topvenue1').click(function(){
-        $( "#topvenue1" ).submit();
-    })
-    $('#topvenue2').click(function(){
-        $( "#topvenue2" ).submit();
-    })
-    $('#topvenue3').click(function(){
-        $( "#topvenue3" ).submit();
-    })
+
 
 });
 
